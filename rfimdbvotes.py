cf13c514037a7d01e854495670df1f468054f7ac
Annej2026 <53411273+Annej2026@users.noreply.github.com>
2023-10-05 02:25:54 +1100

    Update rfimdbvotes.py

--- rfimdbvotes.py
+++ rfimdbvotes.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 import ast
 
+#preprosses data similarily
 train = pd.read_csv('movie_data_version1.csv')
 y_label = 'imdb_votes'
 label = train[y_label]
@@ -15,16 +16,19 @@ data_encoded = data_encoded.drop(columns=['genres'])
 data_encoded.fillna(data_encoded.mean(), inplace=True)
 label.fillna(label.mean(), inplace=True)
 
+#model
 model = RandomForestRegressor(n_estimators=100, random_state=42)
 model.fit(data_encoded, label)
 
+#collect importances
 feature_importances = model.feature_importances_
 
+#sort to sellect top 50
 sorted_indices = feature_importances.argsort()[::-1]
 sorted_importances = feature_importances[sorted_indices]
 sorted_feature_names = data_encoded.columns[sorted_indices]
 
-# Create a bar plot for feature importances
+#create a graph
 plt.figure(figsize=(10, 6))
 plt.barh(sorted_feature_names[:50], sorted_importances[:50])
 plt.xlabel('Feature Importance')
@@ -33,6 +37,5 @@ plt.title('Top 50 Feature Importances (Random Forest)')
 plt.gca().invert_yaxis()
 plt.tight_layout()
 
-# Save the figure for top 50 feature importances as an image
 plt.savefig('top_50_feature_importances_rf.png', bbox_inches='tight')
 plt.close()
