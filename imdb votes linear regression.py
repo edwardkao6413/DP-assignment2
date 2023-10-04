3389da299e56df07f8b2fac80ef84bb687d9cf01
Annej2026 <53411273+Annej2026@users.noreply.github.com>
2023-10-05 02:24:47 +1100

    Update and rename project.py to imdb votes linear regression.py

--- imdb votes linear regression.py
+++ imdb votes linear regression.py
@@ -5,6 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import ast
 
+#importing
 train = pd.read_csv('movie_data_version1.csv')
 y_label = 'imdb_votes'
 label = train[y_label]
@@ -13,7 +14,7 @@ data = train.drop(columns=[y_label])
 #research question does not include analysis of age certification
 data = train.drop(columns= 'age_certification')
 
-#encoding genre feature
+#encoding genre feature as genre is currently a category
 data['genres'] = data['genres'].apply(ast.literal_eval)
 encoded_genres = data['genres'].apply(pd.Series).stack().str.get_dummies().sum(level=0)
 data_encoded = pd.concat([data, encoded_genres], axis=1)
@@ -23,13 +24,16 @@ data_encoded = data_encoded.drop(columns=[y_label])
 
 label.fillna(label.mean(), inplace=True)  # Replace NaN values in label with column mean
 
+#apply linear regression
 model = LinearRegression()
 model.fit(data_encoded, label)
 
+#collect coefficients to compare
 coefficients = model.coef_
 print("Coefficient weight; ", coefficients)
 feature_names = data_encoded.columns
 
+#graph all coefficients
 plt.figure(figsize=(10, 6))
 plt.barh(feature_names, coefficients)
 plt.xlabel('Coefficient Value')
@@ -46,6 +50,7 @@ plt.show()
 top_50_coefficients = coefficients.argsort()[-50:][::-1]
 bottom_50_coefficients = coefficients.argsort()[:50]
 
+#graph top 50
 plt.figure(figsize=(10, 6))
 plt.barh(feature_names[top_50_coefficients], coefficients[top_50_coefficients])
 plt.xlabel('Coefficient Value')
@@ -54,11 +59,10 @@ plt.title('Top 50 Linear Regression Coefficients')
 plt.gca().invert_yaxis()
 plt.tight_layout()
 
-# Save the figure for top 50 coefficients as an image
 plt.savefig('top_50_coefficients_imdb_votes.png', bbox_inches='tight')
 plt.close()
 
-# Create a figure for the bottom 50 coefficients
+#graph bottom 50 coefficients
 plt.figure(figsize=(10, 6))
 plt.barh(feature_names[bottom_50_coefficients], coefficients[bottom_50_coefficients])
 plt.xlabel('Coefficient Value')
@@ -67,7 +71,6 @@ plt.title('Bottom 50 Linear Regression Coefficients')
 plt.gca().invert_yaxis()
 plt.tight_layout()
 
-# Save the figure for bottom 50 coefficients as an image
 plt.savefig('bottom_50_coefficients_imdb_votes.png', bbox_inches='tight')
 plt.close()
 
